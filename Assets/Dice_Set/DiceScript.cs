8821a71bc3ba084eb49ee79e5742ed5a4317ff1f
Carles Homs <36209557+ch0m5@users.noreply.github.com>
2020-05-16 19:45:55 +0200

    Dice Progress and Fonts Folder

--- Assets/Dice_Set/DiceScript.cs
+++ Assets/Dice_Set/DiceScript.cs
@@ -1,40 +1,68 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DiceScript : MonoBehaviour {
 
 	static Rigidbody rb;
-	public static Vector3 diceVelocity;
 	private float heightTreshold;
+	private List<GameObject> sides = new List<GameObject>();
 
 	private float thresholdCrossedAt;
 	private float groundTimer = 0.5f;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		rb = GetComponent<Rigidbody>();
-		heightTreshold = 0.02f;//GameObject.Find("Bowl_Wall_4").transform.position.y + 0.02f;
+		heightTreshold = GameObject.Find("Bowl_Wall_0").transform.position.y + 0.02f;
+
+		int i = 1;
+		bool listFinished = false;
+
+		while (!listFinished)
+		{
+			GameObject tmpGO = GameObject.Find(gameObject.name + "_Side_" + i++);
+			if (tmpGO != null)
+				sides.Add(tmpGO);
+			else
+				listFinished = true;
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		diceVelocity = rb.velocity;
-
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			rb.isKinematic = false;
 			thresholdCrossedAt = 0f;
 
-			//DiceNumberTextScript.diceNumber = 0;
 			transform.localPosition = new Vector3(0, 0.2f, 0);
+			transform.rotation = UnityEngine.Random.rotation;
+
 			rb.AddForce(Vector3.up * 0.5f, ForceMode.Impulse);
 			rb.AddForce(transform.up * 0.1f, ForceMode.Impulse);
-			rb.AddTorque(new Vector3(Random.Range(0, 500), Random.Range(0, 500), Random.Range(0, 500)), ForceMode.Impulse);
+			rb.AddTorque(new Vector3(UnityEngine.Random.Range(0, 500), UnityEngine.Random.Range(0, 500), UnityEngine.Random.Range(0, 500)), ForceMode.Impulse);
 		}
 
-		if (thresholdCrossedAt == 0f && transform.position.y < heightTreshold)
-			thresholdCrossedAt = Time.time;
-		else if (thresholdCrossedAt != 0f && Time.time - thresholdCrossedAt > groundTimer && rb.isKinematic == false)
+		if (thresholdCrossedAt == 0f)
+		{
+			if (transform.position.y < heightTreshold)
+				thresholdCrossedAt = Time.time;
+		}
+		else if (Time.time - thresholdCrossedAt > groundTimer && rb.isKinematic == false)
+		{
+			GameObject highest = sides[0];
+
+			foreach (GameObject go in sides)
+				if (go.transform.position.y > highest.transform.position.y)
+					highest = go;
+
+			Text text = GameObject.Find("Roll_Result").GetComponent<Text>();
+
+			text.text = "You rolled a " + highest.name.Remove(0, 9);
 			rb.isKinematic = true;
+		}
 	}
 }
